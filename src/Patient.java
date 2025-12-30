c4cc0ad7e1cec4546e13bf771ebb3373e0bda859
leaa34 <leaawada13@gmail.com>
2025-12-30 22:33:38 +0200

    Initial Patient class setup.

---
+++ src/Patient.java
@@ -0,0 +1,28 @@
+
+public class Patient {
+ private int id;
+ private String name;
+ private int severity;
+ 
+ public Patient(int id, String name, int severity) {
+	 this.id= id;
+	 this.name = name;
+	 this.severity= severity;
+ }
+ public int getId() {
+	 return id;
+ }
+ 
+ public String getName() {
+	 return name;
+ }
+ public int getSeverity() {
+	 return severity;
+ }
+ 
+ @Override
+ public String toString() {
+	 return "[Priority " + severity+ "] ID: "+ id+" - Name: "+ name; 
+ }
+ 
+}
